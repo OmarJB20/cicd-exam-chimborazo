b050374f17680c8150b055efe8bb87354997cb3f
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2025-11-26 10:15:20 -0500

    Proyecto examen CI/CD: app Flask + tests + Docker + stack

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+# Mini IA: responde un texto aleatorio (simulación simple)
+responses = [
+    "Hola, soy una IA básica lista para ayudarte.",
+    "Procesando tu solicitud...",
+    "Estoy funcionando correctamente dentro del contenedor."
+]
+
+@app.route("/")
+def home():
+    return "API Flask con IA operativa (v1.0.5)"
+
+@app.route("/ia", methods=["POST"])
+def ia():
+    data = request.json
+    pregunta = data.get("pregunta", "")
+    return jsonify({
+        "pregunta": pregunta,
+        "respuesta": random.choice(responses)
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
